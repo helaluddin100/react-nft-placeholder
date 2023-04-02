68c710664137dfd8a68e00a0b3fe631725b44d29
Helal Uddin <freelancerhelaluddin@gmail.com>
2023-04-02 14:06:04 +0600

    update file formate

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Home from "./components/Home";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 const App = () => {
-  return <h1>Hello GiVi</h1>;
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        <Route path="/" exact component={Home} />
+      </Switch>
+      <Footer />
+    </Router>
+  );
 };
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <nav className="navigation">
+        <header></header>
+      </nav>
+    </div>
+  );
+}
+
+export default Header;
